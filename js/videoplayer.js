3ccb90d3694fc8f6245cd8818056f6dcf13e4b6a
pankajan <pankajan05@gmail.com>
2021-08-30 11:47:52 +0530

    add mobile responsiveness

---
+++ js/videoplayer.js
@@ -0,0 +1,16 @@
+
+var video = document.getElementById("myVideo");
+
+// Get the button
+var btn = document.getElementById("myBtn");
+
+// Pause and play the video, and change the button text
+function myFunction() {
+    if (video.paused) {
+        video.play();
+        btn.innerHTML = "Pause";
+    } else {
+        video.pause();
+        btn.innerHTML = "Play";
+    }
+}
