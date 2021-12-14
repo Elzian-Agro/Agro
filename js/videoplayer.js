27f36082ce65dddeb7ad6389a5226be4a8d25a8e
RimazRizkan <rimaz660066@gmail.com>
2021-12-14 22:08:45 +0530

    Minified most of the JS files

--- js/videoplayer.js
+++ js/videoplayer.js
@@ -1,17 +1,1 @@
-
-var video = document.getElementById("myVideo");
-
-// Get the button
-var btn = document.getElementById("myBtn");
-
-// Pause and play the video, and change the button text
-function myFunction() {
-    if (video.paused) {
-        video.play();
-        btn.innerHTML = "Pause";
-    } else {
-        video.pause();
-        btn.innerHTML = "Play";
-    }
-}
-
+var video=document.getElementById("myVideo"),btn=document.getElementById("myBtn");function myFunction(){video.paused?(video.play(),btn.innerHTML="Pause"):(video.pause(),btn.innerHTML="Play")}
