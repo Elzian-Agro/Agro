9266cce7669a585a9d28e8425e1e93352993dfd4
Bagheerathan <70306720+Bagheerathan@users.noreply.github.com>
2023-02-24 22:10:42 +0530

    technology section

---
+++ js/tech.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $(".customer-logos").slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 520,
+                settings: {
+                    slidesToShow: 2
+                }
+            }
+        ]
+    });
+});
