e2dccf9425cadff13c8cbe20cab2b2c6e06606ec
danushb151 <danushkab@horizoncampus.edu.lk>
2023-06-26 15:33:14 +0530

    blogs updated

--- js/blog-share.js
+++ js/blog-share.js
@@ -5,4 +5,20 @@ function toggle() {
     var popup = document.getElementById("popup");
     popup.classList.toggle("active");
   }
+
+  document.querySelectorAll(".copy-link").forEach((copyLinkParent) => {
+    const inputField = copyLinkParent.querySelector(".copy-link-input");
+    const copyButton = copyLinkParent.querySelector(".copy-link-button");
+    const text = inputField.value;
+  
+    inputField.addEventListener("focus", () => inputField.select());
+  
+    copyButton.addEventListener("click", () => {
+      inputField.select();
+      navigator.clipboard.writeText(text);
+  
+      inputField.value = "Copied!";
+      setTimeout(() => (inputField.value = text), 2000);
+    });
+  });
   
