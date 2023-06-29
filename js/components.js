965c12ab168546beb8d219715583d7e08a7f8f26
Suresh_Gobi <sureshgobi34@gmail.com>
2023-06-29 23:18:03 +0530

    Dynamic Header and Footer

---
+++ js/components.js
@@ -0,0 +1,20 @@
+// Fetch and insert the navigation bar
+fetch('../components/navbar.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('navbar-placeholder').innerHTML = data;
+  });
+
+// Fetch and insert the pre-loader
+fetch('../components/pre-loader.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('pre-loader-placeholder').innerHTML = data;
+  });
+
+// Fetch and insert the footer
+fetch('../components/footer.html')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById('footer-placeholder').innerHTML = data;
+  });
